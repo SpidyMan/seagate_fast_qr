2ffe671f0acdc26b0be16ec6022876d26a18bd46
jamecmos@gmail.com <wiriya.choeiprathum@seagate.com>
2020-05-12 14:50:29 +0700

    rev2

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -77,7 +77,7 @@ class _HomeState extends State<Home> {
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
           helperText: 'xxxxtype in your GID',
         ),
-//test XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx
+//test yyyyyyyyyyyyyyyyyyyyyyyyyy
         validator: (String value) {
           if (value.isEmpty) {
             return 'Please check your GID';
